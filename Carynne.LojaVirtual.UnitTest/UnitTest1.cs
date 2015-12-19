eaf0201e41dd4a18004e078a1d7c339d476d41c2
Douglas Godoi Ribeiro <douglas.ribeiro@tcs.com>
2015-12-19 16:22:22 -0200

    First Commit Loja.

---
+++ Carynne.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Carynne.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestCarynne
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
