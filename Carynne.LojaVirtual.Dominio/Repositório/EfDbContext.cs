22547469c822b38f6b1e8d8838a76bbdd5cae59e
Douglas Godoi Ribeiro <douglas.ribeiro@tcs.com>
2015-12-20 22:12:34 -0200

    Adicionado EntityFramework e View de Produtos

---
+++ Carynne.LojaVirtual.Dominio/Repositório/EfDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Carynne.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carynne.LojaVirtual.Dominio.Repositório
+{
+    public class EfDbContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+            //base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Carynne.LojaVirtual.Dominio/Repositório/ProdutosRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using Carynne.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carynne.LojaVirtual.Dominio.Repositório
+{
+    public class ProdutosRepositorio
+    {
+        private readonly EfDbContext context = new EfDbContext();
+
+        public IEnumerable<Produto> Produtos {
+            get {
+                return context.Produtos;
+            }
+        }
+    }
+}
