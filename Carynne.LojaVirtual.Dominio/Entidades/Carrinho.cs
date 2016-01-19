b8aee70982303855ee52cd919e172e414dc05e9d
douglasgodoi <douglas-godoi@outlook.com>
2016-01-19 20:09:17 -0200

    Carrinho e Item carrinho

---
+++ Carynne.LojaVirtual.Dominio/Entidades/Carrinho.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carynne.LojaVirtual.Dominio.Entidades
+{
+    public class Carrinho
+    {
+        private readonly List<ItemCarrinho> _itensCarrinho = new List<ItemCarrinho>();
+        
+        //Adicionar
+        public void AdicionarItem(Produto produto, int quantidade)
+        {
+            ItemCarrinho item = _itensCarrinho.FirstOrDefault(p => p.Produto.ProdutoId.Equals(produto.ProdutoId));
+
+            if(item == null)
+            {
+                _itensCarrinho.Add(new ItemCarrinho
+                {
+                    Produto = produto,
+                    Quantidade = quantidade
+                });
+            }
+            else
+            {
+                quantidade += quantidade;
+            }
+        }
+        
+        //Remover
+        public void RemoverItem(Produto produto)
+        {
+            _itensCarrinho.RemoveAll(i => i.Produto.ProdutoId.Equals(produto.ProdutoId));
+        }
+
+        //Obter valor total
+        public decimal ObterValorTotal()
+        {
+            return _itensCarrinho.Sum(e => e.Produto.Preco * e.Quantidade);
+        }
+
+        //Limpar carrinho
+        public void LimparCarrinho()
+        {
+            _itensCarrinho.Clear();
+        }
+
+        //Items carrinho
+        public IEnumerable<ItemCarrinho> ItemsCarrinho
+        {
+            get { return _itensCarrinho; }
+        }
+
+    }
+    public class ItemCarrinho
+    {
+        public Produto Produto { get; set; }
+
+        public int Quantidade { get; set; }
+    }
+}
