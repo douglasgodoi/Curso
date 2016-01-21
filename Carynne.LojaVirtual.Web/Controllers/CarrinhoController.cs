4c81474eaabf13e8554e8d3b476383cdeeec9a8f
douglasgodoi <douglas-godoi@outlook.com>
2016-01-20 22:31:03 -0200

    Desenvolvimento do Carrinho. 18 e 19.

---
+++ Carynne.LojaVirtual.Web/Controllers/CarrinhoController.cs
@@ -0,0 +1,66 @@
+﻿using Carynne.LojaVirtual.Dominio.Entidades;
+using Carynne.LojaVirtual.Dominio.Repositório;
+using Carynne.LojaVirtual.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Carynne.LojaVirtual.Web.Controllers
+{
+    public class CarrinhoController : Controller
+    {
+        private ProdutosRepositorio _repositorio;
+
+        private Carrinho ObterCarrinho()
+        {
+            Carrinho carrinho = (Carrinho)Session["Carrinho"];
+            if (carrinho == null)
+            {
+                carrinho = new Carrinho();
+                Session["Carrinho"] = carrinho;
+            }
+
+            return carrinho;
+        }
+
+        // GET: Carrinho
+        public RedirectToRouteResult Adicionar(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            Produto produto = _repositorio.Produtos.FirstOrDefault(p => p.ProdutoId.Equals(produtoId));
+
+            if(produto != null)
+            {
+                ObterCarrinho().AdicionarItem(produto, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult Remover(int produtoId, string returnUrl)
+        {
+            _repositorio = new ProdutosRepositorio();
+
+            Produto produto = _repositorio.Produtos.FirstOrDefault(p => p.ProdutoId.Equals(produtoId));
+
+            if(produto != null)
+            {
+                ObterCarrinho().RemoverItem(produto);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CarrinhoViewModel
+            {
+                Carrinho = ObterCarrinho(),
+                ReturnUrl = returnUrl
+            });
+        }
+    }
+}
---
+++ Carynne.LojaVirtual.Web/Models/CarrinhoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Carynne.LojaVirtual.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Carynne.LojaVirtual.Web.Models
+{
+    public class CarrinhoViewModel
+    {
+        public Carrinho Carrinho { get; set; }
+
+        public string ReturnUrl { get; set; }
+    }
+}
